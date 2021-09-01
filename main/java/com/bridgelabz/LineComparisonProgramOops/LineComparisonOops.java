43802c41993758fed3557877abe39578beff76be
sonam1812 <sonampal832@gmail.com>
2021-09-01 19:53:16 +0530

    Added: Compare line

--- main/java/com/bridgelabz/LineComparisonProgramOops/LineComparisonOops.java
+++ main/java/com/bridgelabz/LineComparisonProgramOops/LineComparisonOops.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class LineComparisonOops {
 	public static void main(String[] args) {
-
+//for line1 input
 		        Scanner sc = new Scanner(System.in);
 		        System.out.print("Enter x1 of line 1:");
 		        int x1 = sc.nextInt();
@@ -16,7 +16,7 @@ public class LineComparisonOops {
 
 		        System.out.print("Enter y2 of line 1:");
 		        int y2 = sc.nextInt();
-
+//for line2 input
 		        System.out.print("Enter x1 of line 2:");
 		        int p1 = sc.nextInt();
 
@@ -39,15 +39,19 @@ public class LineComparisonOops {
 		        int a = x2 - x1;
 		        int b = y2 - y1;
 		        double length = Math.sqrt((a * a) + (b * b));
-		        System.out.println("Distance between the given coordinates are : " + length);
+		        System.out.println("Distance between the coordinates are : " + length);
 		        return length;
 		    }
 
 		    public void compare(double length1, double length2) {
-		        if (length1 == length2) {
-		            System.out.println("Both lines are equal");
-		        }
-		    }
+		    	 if(length1 > length2) {
+		             System.out.println("1st line is greater");
+		         }else if(length1==length2) {
+		             System.out.println("Both lines are equal");
+		         }else {
+		             System.out.println("2nd line is greater");
+		         }
+		     }
 	}
 
 
