6a677aa2647f384aade34ff952f485ffe6c1d40b
sonam1812 <sonampal832@gmail.com>
2021-09-01 19:21:57 +0530

    Welcome to LineComparisonProgram

---
+++ main/java/com/bridgelabz/LineComparisonProgramOops/LineComparisonOops.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.LineComparisonProgramOops;
+
+public class LineComparisonOops {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Oops Program ");
+		}
+
+}
